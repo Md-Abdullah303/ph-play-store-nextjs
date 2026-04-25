2b6e1ca45981386f30274883825b8a78feed8dff
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-04-25 16:43:10 +0600

    installed page work is start and installed app cach korao sesh

--- src/app/installation/page.jsx
+++ src/app/installation/page.jsx
@@ -1,9 +1,27 @@
+'use client'
+import { InstalledAppContext } from "@/context/InstallAppProvider/InstallAppProvider";
+import { use, useContext } from "react";
+
 
 
 const InstallationPage = () => {
+
+    const {installApp} = use(InstalledAppContext)
+    console.log(installApp);
     return (
-        <div>
-            <h1>hi i am installation page</h1>
+        <div className="w-[70%] sm:container mx-auto py-20 px-10">
+            <h1 className="text-center text-2xl sm:text-5xl font-bold">Your Install Apps</h1>
+
+            <div className="">
+                <h3 className="text-xl sm:text-3xl">Installed App : {installApp.length}</h3>
+                <div className="">
+                    {
+                        installApp.length === 0 ? 
+                        "no data" :
+                        "data dekhaitasi"
+                    }
+                </div>
+            </div>
         </div>
     );
 };
